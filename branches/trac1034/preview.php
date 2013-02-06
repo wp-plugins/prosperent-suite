5fbeda4ee94165a80ef8e8c81c76c70fa576bf80
Prosperent <Prosperent@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-02-06 18:58:42 +0000

    new tool added, Auto-Comparer, fallbacks for auto-linker and auto-comparer and short code placement
    git-svn-id: https://plugins.svn.wordpress.org/prosperent-suite@664464 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- branches/trac1034/preview.php
+++ branches/trac1034/preview.php
@@ -16,13 +16,21 @@
 		'query'          => $_GET['q'],
 		'visitor_ip'     => $_SERVER['REMOTE_ADDR'],
 		'limit'          => 1,
-		'groupBy'	     => 'productId',
-		'filterMerchant' => $_GET['merchant'],
-		'filterBrand'	 => $_GET['brand']
+		'sortPrice'		 => '',
+		'filterMerchant' => $_GET['merchant']
 	));
 
-	$prosperentApi -> fetch();
-	$results = $prosperentApi -> getAllData();
+	if (!$_GET['coup'])
+	{
+		$prosperentApi -> set_filterBrand($_GET['brand']);
+		$prosperentApi -> fetch();
+		$results = $prosperentApi -> getAllData();
+	}
+	else
+	{
+		$prosperentApi -> fetchCoupons();
+		$coupons = $prosperentApi -> getAllData();
+	}
 
 	if ($results)
 	{
@@ -63,6 +71,73 @@
 			</div>
 		<?php
 		}
+	}
+	else if ($coupons)
+	{
+		?>
+	   <div id="couponList">
+			<?php
+			// Loop to return coupons and corresponding information
+			foreach ($coupons as $i => $record)
+			{
+				?>
+				<div class="couponBlock">
+					<div class="couponImage">
+						<?php
+						echo '<a href="' . $record['affiliate_url'] . '"><img src="' . $record['image_url'] . '" style="background: none repeat scroll 0 0 transparent; border: medium none;"></a>';
+						?>
+					</div>
+					<div class="couponContent">
+						<div class="couponTitle">
+							<?php
+							echo '<a href="' . $record['affiliate_url'] . '">' . $record['keyword'] . '</a>';
+							?>
+						</div>
+						<?php
+						if(!empty($record['expiration_date']))
+						{
+							$expires = strtotime($record['expiration_date']);
+							$today = strtotime(date("Y-m-d"));
+							$interval = abs($expires - $today) / (60*60*24);
+
+							if ($interval <= 7 && $interval > 0)
+							{
+								echo '<div class="couponExpire" style=""><span style="color:red; font-weight:bold;">Expires in ' . $interval . ' days!</span></div>';
+							}
+							else
+							{
+								echo '<div class="couponExpire"><span style="color:red; font-weight:bold;">Expires Soon!</span></div>';
+							}
+						}
+						?>
+						<div class="couponDescription">
+							<?php 
+							if (strlen($record['description']) > 200)
+							{ 
+								echo substr($record['description'], 0, 175) . '...';
+							}
+							else
+							{
+								echo $record['description'];
+							}
+							?>
+						</div>
+						<?php
+						if ($record['coupon_code'])
+						{
+							echo '<div class="couponCode" style="font-weight:bold; padding:5px 0 0 0;">Coupon Code: <span class="code_cc" style="border:2px dashed #3079ed; padding: 2px 3px; font-weight:bold; -moz-border-radius: 8px; -webkit-border-radius: 8px; border-radius:8px;">' . $record['coupon_code'] . '</span></div>';
+						}
+						?>
+					</div>
+					<div class="couponVisit">
+						<a href="<?php echo $record['affiliate_url']; ?>"><img style="background: none repeat scroll 0 0 transparent; border: medium none; box-shadow: none;" src="<?php echo plugins_url('/img/visit_store_button.png', __FILE__);?> "></a>
+					</div>
+				</div>
+				<?php
+			}
+			?>
+		</div>
+		<?php
 	}
 	else
 	{

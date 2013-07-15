5aad3fc06433eadb1dce1abad6e224b467d7072a
Prosperent <Prosperent@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-07-15 17:34:24 +0000

    bug fixes, added open graph rules, fixed page titles
    git-svn-id: https://plugins.svn.wordpress.org/prosperent-suite/trunk@741190 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- compare_short.php
+++ compare_short.php
@@ -14,18 +14,18 @@ $prosperentApi = new Prosperent_Api(array(
 
 switch ($options['Country'])
 {
-	case 'UK':
-		$prosperentApi -> fetchUkProducts();
-		$currency = 'GBP';
-		break;
-	case 'CA':
-		$prosperentApi -> fetchCaProducts();
-		$currency = 'CAD';
-		break;
-	default:
-		$prosperentApi -> fetchProducts();
-		$currency = 'USD';
-		break;
+    case 'UK':
+        $prosperentApi -> fetchUkProducts();
+        $currency = 'GBP';
+        break;
+    case 'CA':
+        $prosperentApi -> fetchCaProducts();
+        $currency = 'CAD';
+        break;
+    default:
+        $prosperentApi -> fetchProducts();
+        $currency = 'USD';
+        break;
 }
 $result = $prosperentApi -> getAllData();
 if (count($result) > 1)
@@ -35,62 +35,62 @@ if (count($result) > 1)
         <?php
         foreach ($result as $record)
         {
-            $record['image_url'] = $productPage  . '/img/'. urlencode(str_replace(array('http://img1.prosperent.com/images/', '/'), array('', ',SL,'), preg_replace('/\/250x250\//', '/125x125/', $record['image_url'])));
-			?>
-			<div class="<?php echo count($results) >= 2 ? 'productBlock' : 'productBlock0'; ?>">
-				<div class="productImage">
-					<a href="<?php echo $productPage . '/product/' . urlencode(str_replace('/', ',SL,', $record['keyword'])) . '/cid/' . $record['catalogId']; ?>"><span><img src="<?php echo $record['image_url']; ?>"  title="<?php echo $record['keyword']; ?>" style="background: none repeat scroll 0 0 transparent; border: medium none;"></span></a>
-				</div>
-				<div class="productContent">
-					<div class="productTitle"><a href="<?php echo $productPage . '/product/' . urlencode(str_replace('/', ',SL,', $record['keyword'])) . '/cid/' . $record['catalogId']; ?>"><span><?php echo $record['keyword']; ?></span></a></div>
-					<div class="productDescription"><?php
-						if (strlen($record['description']) > 200)
-						{
-							echo substr($record['description'], 0, 200) . '...';
-						}
-						else
-						{
-							echo $record['description'];
-						}
-						?>
-					</div>
-					<div class="productBrandMerchant">
-						<?php
-						if($record['brand'] && !$filterBrand)
-						{
-							echo '<span class="brandIn"><u>Brand</u>: <a href="' . $prodSubmit . '/brand/' . urlencode($record['brand']) . '"><cite>' . $record['brand'] . '</cite></a></span>';
-						}
-						if($record['merchant'] && !$filterMerchant)
-						{
-							echo '<span class="merchantIn"><u>Merchant</u>: <a href="' . $prodSubmit . '/merchant/' . urlencode($record['merchant']) . '"><cite>' . $record['merchant'] . '</cite></a></span>';
-						}
-						?>
-					</div>
-				</div>
-				<div class="productEnd">
-					<?php
-					if(empty($record['price_sale']) || $record['price'] <= $record['price_sale'])
-					{
-						//we don't do anything
-						?>
-						<div class="productPriceNoSale"><span><?php echo '$' . $record['price']; ?></span></div>
-						<?php
-					}
-					//otherwise strike-through Price and list the Price_Sale
-					else
-					{
-						?>
-						<div class="productPrice"><span><?php echo ($currency == 'GBP' ? '&pound;' : '$') . $record['price']?></span></div>
-						<div class="productPriceSale"><span><?php echo ($currency == 'GBP' ? '&pound;' : '$') . $record['price_sale']?></span></div>
-						<?php
-					}
-					?>
-					<form style="margin:0;" action="<?php echo $productPage . '/store/go/' . urlencode(str_replace(array('http://prosperent.com/', '/'), array('', ',SL,'), $record['affiliate_url'])) . '" target="' . $target; ?>" method="POST">
-						<input type="submit" value="Visit Store"/>
-					</form>
-				</div>
-			</div>
-			<?php
+            $record['image_url'] = $options['Image_Masking'] ? $productPage  . '/img/'. urlencode(str_replace(array('http://img1.prosperent.com/images/', '/'), array('', ',SL,'), preg_replace('/\/250x250\//', '/125x125/', $record['image_url']))) : preg_replace('/\/250x250\//', '/125x125/', $record['image_url']);
+            ?>
+            <div class="<?php echo count($results) >= 2 ? 'productBlock' : 'productBlock0'; ?>">
+                <div class="productImage">
+                    <a href="<?php echo $productPage . '/product/' . urlencode(str_replace('/', ',SL,', $record['keyword'])) . '/cid/' . $record['catalogId']; ?>"><span><img src="<?php echo $record['image_url']; ?>"  title="<?php echo $record['keyword']; ?>" style="background: none repeat scroll 0 0 transparent; border: medium none;"></span></a>
+                </div>
+                <div class="productContent">
+                    <div class="productTitle"><a href="<?php echo $productPage . '/product/' . urlencode(str_replace('/', ',SL,', $record['keyword'])) . '/cid/' . $record['catalogId']; ?>"><span><?php echo $record['keyword']; ?></span></a></div>
+                    <div class="productDescription"><?php
+                        if (strlen($record['description']) > 200)
+                        {
+                            echo substr($record['description'], 0, 200) . '...';
+                        }
+                        else
+                        {
+                            echo $record['description'];
+                        }
+                        ?>
+                    </div>
+                    <div class="productBrandMerchant">
+                        <?php
+                        if($record['brand'] && !$filterBrand)
+                        {
+                            echo '<span class="brandIn"><u>Brand</u>: <a href="' . $prodSubmit . '/brand/' . urlencode($record['brand']) . '"><cite>' . $record['brand'] . '</cite></a></span>';
+                        }
+                        if($record['merchant'] && !$filterMerchant)
+                        {
+                            echo '<span class="merchantIn"><u>Merchant</u>: <a href="' . $prodSubmit . '/merchant/' . urlencode($record['merchant']) . '"><cite>' . $record['merchant'] . '</cite></a></span>';
+                        }
+                        ?>
+                    </div>
+                </div>
+                <div class="productEnd">
+                    <?php
+                    if(empty($record['price_sale']) || $record['price'] <= $record['price_sale'])
+                    {
+                        //we don't do anything
+                        ?>
+                        <div class="productPriceNoSale"><span><?php echo '$' . $record['price']; ?></span></div>
+                        <?php
+                    }
+                    //otherwise strike-through Price and list the Price_Sale
+                    else
+                    {
+                        ?>
+                        <div class="productPrice"><span><?php echo ($currency == 'GBP' ? '&pound;' : '$') . $record['price']?></span></div>
+                        <div class="productPriceSale"><span><?php echo ($currency == 'GBP' ? '&pound;' : '$') . $record['price_sale']?></span></div>
+                        <?php
+                    }
+                    ?>
+                    <form style="margin:0;" action="<?php echo $productPage . '/store/go/' . urlencode(str_replace(array('http://prosperent.com/', '/'), array('', ',SL,'), $record['affiliate_url'])) . '" target="' . $target; ?>" method="POST">
+                        <input type="submit" value="Visit Store"/>
+                    </form>
+                </div>
+            </div>
+            <?php
         }
         ?>
     </div>
@@ -103,62 +103,62 @@ else
         <?php
         foreach ($results as $record)
         {
-			$record['image_url'] = $productPage  . '/img/'. urlencode(str_replace(array('http://img1.prosperent.com/images/', '/'), array('', ',SL,'), preg_replace('/\/250x250\//', '/125x125/', $record['image_url'])));
-			?>
-			<div class="productBlock0">
-				<div class="productImage">
-					<a href="<?php echo $productPage . '/product/' . urlencode(str_replace('/', ',SL,', $record['keyword'])) . '/cid/' . $record['catalogId']; ?>"><span><img src="<?php echo $record['image_url']; ?>"  title="<?php echo $record['keyword']; ?>" style="background: none repeat scroll 0 0 transparent; border: medium none;"></span></a>
-				</div>
-				<div class="productContent">
-					<div class="productTitle"><a href="<?php echo $productPage . '/product/' . urlencode(str_replace('/', ',SL,', $record['keyword'])) . '/cid/' . $record['catalogId']; ?>"><span><?php echo $record['keyword']; ?></span></a></div>
-					<div class="productDescription"><?php
-						if (strlen($record['description']) > 200)
-						{
-							echo substr($record['description'], 0, 200) . '...';
-						}
-						else
-						{
-							echo $record['description'];
-						}
-						?>
-					</div>
-					<div class="productBrandMerchant">
-						<?php
-						if($record['brand'])
-						{
-							echo '<span class="brandIn"><u>Brand</u>: <a href="' . $prodSubmit . '/brand/' . urlencode($record['brand']) . '"><cite>' . $record['brand'] . '</cite></a></span>';
-						}
-						if($record['merchant'])
-						{
-							echo '<span class="merchantIn"><u>Merchant</u>: <a href="' . $prodSubmit . '/merchant/' . urlencode($record['merchant']) . '"><cite>' . $record['merchant'] . '</cite></a></span>';
-						}
-						?>
-					</div>
-				</div>
-				<div class="productEnd">
-					<?php
-					if(empty($record['price_sale']) || $record['price'] <= $record['price_sale'])
-					{
-						//we don't do anything
-						?>
-						<div class="productPriceNoSale"><span><?php echo '$' . $record['price']; ?></span></div>
-						<?php
-					}
-					//otherwise strike-through Price and list the Price_Sale
-					else
-					{
-						?>
-						<div class="productPrice"><span><?php echo ($currency == 'GBP' ? '&pound;' : '$') . $record['price']?></span></div>
-						<div class="productPriceSale"><span><?php echo ($currency == 'GBP' ? '&pound;' : '$') . $record['price_sale']?></span></div>
-						<?php
-					}
-					?>
-					<form style="margin:0;" action="<?php echo $productPage . '/store/go/' . urlencode(str_replace(array('http://prosperent.com/', '/'), array('', ',SL,'), $record['affiliate_url'])) . '" target="' . $target; ?>" method="POST">
-						<input type="submit" value="Visit Store"/>
-					</form>
-				</div>
-			</div>
-			<?php
+            $record['image_url'] = $options['Image_Masking'] ? $productPage  . '/img/'. urlencode(str_replace(array('http://img1.prosperent.com/images/', '/'), array('', ',SL,'), preg_replace('/\/250x250\//', '/125x125/', $record['image_url']))) : preg_replace('/\/250x250\//', '/125x125/', $record['image_url']);
+            ?>
+            <div class="productBlock0">
+                <div class="productImage">
+                    <a href="<?php echo $productPage . '/product/' . urlencode(str_replace('/', ',SL,', $record['keyword'])) . '/cid/' . $record['catalogId']; ?>"><span><img src="<?php echo $record['image_url']; ?>"  title="<?php echo $record['keyword']; ?>" style="background: none repeat scroll 0 0 transparent; border: medium none;"></span></a>
+                </div>
+                <div class="productContent">
+                    <div class="productTitle"><a href="<?php echo $productPage . '/product/' . urlencode(str_replace('/', ',SL,', $record['keyword'])) . '/cid/' . $record['catalogId']; ?>"><span><?php echo $record['keyword']; ?></span></a></div>
+                    <div class="productDescription"><?php
+                        if (strlen($record['description']) > 200)
+                        {
+                            echo substr($record['description'], 0, 200) . '...';
+                        }
+                        else
+                        {
+                            echo $record['description'];
+                        }
+                        ?>
+                    </div>
+                    <div class="productBrandMerchant">
+                        <?php
+                        if($record['brand'])
+                        {
+                            echo '<span class="brandIn"><u>Brand</u>: <a href="' . $prodSubmit . '/brand/' . urlencode($record['brand']) . '"><cite>' . $record['brand'] . '</cite></a></span>';
+                        }
+                        if($record['merchant'])
+                        {
+                            echo '<span class="merchantIn"><u>Merchant</u>: <a href="' . $prodSubmit . '/merchant/' . urlencode($record['merchant']) . '"><cite>' . $record['merchant'] . '</cite></a></span>';
+                        }
+                        ?>
+                    </div>
+                </div>
+                <div class="productEnd">
+                    <?php
+                    if(empty($record['price_sale']) || $record['price'] <= $record['price_sale'])
+                    {
+                        //we don't do anything
+                        ?>
+                        <div class="productPriceNoSale"><span><?php echo '$' . $record['price']; ?></span></div>
+                        <?php
+                    }
+                    //otherwise strike-through Price and list the Price_Sale
+                    else
+                    {
+                        ?>
+                        <div class="productPrice"><span><?php echo ($currency == 'GBP' ? '&pound;' : '$') . $record['price']?></span></div>
+                        <div class="productPriceSale"><span><?php echo ($currency == 'GBP' ? '&pound;' : '$') . $record['price_sale']?></span></div>
+                        <?php
+                    }
+                    ?>
+                    <form style="margin:0;" action="<?php echo $productPage . '/store/go/' . urlencode(str_replace(array('http://prosperent.com/', '/'), array('', ',SL,'), $record['affiliate_url'])) . '" target="' . $target; ?>" method="POST">
+                        <input type="submit" value="Visit Store"/>
+                    </form>
+                </div>
+            </div>
+            <?php
         }
         ?>
     </div>

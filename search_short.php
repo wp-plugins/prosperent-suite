0050628d0fe7fb91be4d3f3d03617b18c4b03c3c
Prosperent <Prosperent@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-08-20 21:53:10 +0000

    fixed search shortcode
    git-svn-id: https://plugins.svn.wordpress.org/prosperent-suite/trunk@759552 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- search_short.php
+++ search_short.php
@@ -1,19 +1,19 @@
 <?php
-$params = array_reverse(explode('/', get_query_var('queryParams')));			
+$params = array_reverse(explode('/', get_query_var('queryParams')));
 
 $sendParams = array();
 if (!empty($params))
 {
-	$params = array_reverse($params);
-	foreach ($params as $k => $p)
-	{
-		//if the number is even, grab the next index value
-		if (!($k & 1))
-		{
-			$sendParams[$p] = $params[$k + 1];
-		}
-	}
-}	
+    $params = array_reverse($params);
+    foreach ($params as $k => $p)
+    {
+        //if the number is even, grab the next index value
+        if (!($k & 1))
+        {
+            $sendParams[$p] = $params[$k + 1];
+        }
+    }
+}
 
 $query = $sendParams['query'];
 $base = $options['Base_URL'] ? $options['Base_URL'] : 'products';
@@ -23,22 +23,22 @@ $page = !$options['Base_URL'] ? 'products' : $options['Base_URL'];
 
 if (is_page($page) && $_POST['q'])
 {
-	$url = 'http://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
-	$prodSubmit = preg_replace('/\/$/', '', $url);
-	$newQuery = str_replace(array('/query/' . $query, '/query/' . urlencode($query)), array('', ''), $prodSubmit);
-	header('Location: ' . $newQuery . '/query/' . urlencode(trim($_POST['q'])));
-	exit;
-}		
-elseif ($_POST['q']) 
+    $url = 'http://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
+    $prodSubmit = preg_replace('/\/$/', '', $url);
+    $newQuery = str_replace(array('/query/' . $query, '/query/' . urlencode($query)), array('', ''), $prodSubmit);
+    header('Location: ' . $newQuery . '/query/' . urlencode(trim($_POST['q'])));
+    exit;
+}
+elseif ($_POST['q'])
 {
-	header('Location: ' . $url . '/query/' . urlencode(trim($_POST['q'])));
-	exit;
+    header('Location: ' . $url . '/query/' . urlencode(trim($_POST['q'])));
+    exit;
 }
 ?>
 
 <div style="<?php echo $options['Additional_CSS']; ?>">
-	<form class="searchform" method="POST" action="">
-		<input class="field" type="text" name="q" id="s" placeholder="<?php echo !$options['Search_Bar_Text'] ? 'Search Products' : $options['Search_Bar_Text']; ?>">
-		<input class="submit" type="submit" value="Search">
-	</form>
+    <form class="searchform" method="POST" action="">
+        <input class="prosper_field" type="text" name="q" id="s" placeholder="<?php echo !$options['Search_Bar_Text'] ? 'Search Products' : $options['Search_Bar_Text']; ?>">
+        <input class="prosper_submit" type="submit" value="Search">
+    </form>
 </div>

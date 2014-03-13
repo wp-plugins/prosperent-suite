8a35581fe63eddf6357a8004ef539fc23c20db0a
Prosperent <Prosperent@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-03-13 02:56:45 +0000

    = 3.0.6 = * Fixed product search widget, broke it in last update * Changed some wording on settings pages * Changed list view to default view
    git-svn-id: https://plugins.svn.wordpress.org/prosperent-suite@874478 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- trunk/includes/views/prosperadmin/general-phtml.php
+++ trunk/includes/views/prosperadmin/general-phtml.php
@@ -11,11 +11,11 @@ echo $prosperAdmin->textinput( 'Api_Key', __( '<strong>Prosperent API Key</stron
 echo '<p class="prosper_descb">' . __( '', 'prosperent-suite' ) . '</p>';
 
 echo '<h2 class="prosper_h2">' . __( 'Links', 'prosperent-suite' ) . '</h2>';
-echo $prosperAdmin->checkbox( 'Target', __( 'Open Links in New Window or Tab', 'prosperent-suite' ));
-echo '<p class="prosper_descb">' . __( "<strong>Checked</strong> : opens link in a new window or tab <br><strong>Unchecked</strong> : opens link in the same window<br><strong>Will Not Change the Functionality of Performance Ads</strong>", 'prosperent-suite' ) . '</p>';
+echo $prosperAdmin->checkbox( 'Target', __( 'Open Links in New Window/Tab', 'prosperent-suite' ));
+echo '<p class="prosper_descb">' . __( "<strong>Checked</strong> : opens link in a new window/tab <br><strong>Unchecked</strong> : opens link in the same window<br><strong>Will Not Change the Functionality of Performance Ads or ProsperLinks</strong>", 'prosperent-suite' ) . '</p>';
 
 echo '<h2 class="prosper_h2">' . __( 'Caching', 'prosperent-suite' ) . '</h2>';
 echo $prosperAdmin->checkbox( 'Enable_Caching', __( 'Turn on Caching', 'prosperent-suite' ));
-echo '<p class="prosper_desc">' . __( "You will need to create the <strong>prosperent_cache</strong> directory inside your <strong>wp-content</strong> directory and set the permissions to 0777.<br><strong>Note:</strong> By enabling this, you <strong>MUST</strong> have the <strong>prosperent_cache</strong> directory writable inside your <strong>wp-content</strong> directory. <br>If you have this enabled without the directory, <strong>Everything Will Still Work</strong>, just caching will not be used. You will see an <strong>Error</strong> at the top of your page if this is the case.", 'prosperent-suite' ) . '</p>';
+echo '<p class="prosper_desc">' . __( "You will need to create the <strong>prosperent_cache</strong> directory inside your <strong>wp-content</strong> directory and set the permissions to 0777.<br><br><strong>Note:</strong> By enabling this, you <strong>MUST</strong> have the <strong>prosperent_cache</strong> directory writable inside your <strong>wp-content</strong> directory. <br>If you have this enabled without the directory, <strong>Everything Will Still Work</strong>, just caching will not be used. You will see an <strong>Error</strong> at the top of your admin pages if this is the case.", 'prosperent-suite' ) . '</p>';
 
 $prosperAdmin->adminFooter();

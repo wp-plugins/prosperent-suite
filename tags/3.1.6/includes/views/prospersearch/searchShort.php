44ba80a3a79efe03caa062bd5d35a151a9b857f9
Prosperent <Prosperent@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-08-19 22:59:38 +0000

    = 3.1.6 = * Fixed shortcodes for ProsperAds and ProsperInsert * Updated ProsperShop (products, coupons and local) to allow for filtering by category (eg. YOURBLOG.com/shop/category/automotive) * Updated ProsperentAPI
    git-svn-id: https://plugins.svn.wordpress.org/prosperent-suite@968661 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ tags/3.1.6/includes/views/prospersearch/searchShort.php
@@ -0,0 +1,6 @@
+<div style="<?php echo 'width:' . $pieces['w'] . $pieces['ws'] . ';' . $pieces['css']; ?>">
+    <form class="searchform" method="POST" action="" rel="nolink">
+        <input class="prosper_field" type="text" name="q" id="s" placeholder="<?php echo !$options['Search_Bar_Text'] ? 'Search Products' : $options['Search_Bar_Text']; ?>">
+        <input class="prosper_submit" type="submit" value="Search">
+    </form>
+</div>

1c0771fa23ca85d4e848f131ba910cef48824571
Prosperent <Prosperent@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-11-06 00:34:06 +0000

    = 3.2.3 = * Bug Fixes * CSS Fixes
    git-svn-id: https://plugins.svn.wordpress.org/prosperent-suite@1020607 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ tags/3.2.3/includes/models/Ad.php
@@ -0,0 +1,137 @@
+<?php
+require_once(PROSPER_MODEL . '/Base.php');
+/**
+ * Ad Model
+ *
+ * @package Model
+ */
+class Model_Ad extends Model_Base
+{
+	protected $_shortcode = 'perform_ad';
+	
+	protected $_options;
+
+	public function qTagsAd()
+	{	
+		$id 	 = 'performanceAd';
+		$display = 'ProsperAd';
+		$arg1 	 = '[perform_ad q="TOPIC" h="90" w="auto" utt="USE TAGS?" utt="USE TITLE?"]';
+		$arg2 	 = '[/perform_ad]';		
+	
+		$this->qTagsProsper($id, $display, $arg1, $arg2);
+	}
+	
+   /**
+	* Performs shortcode extraction for ProsperAds
+	*
+	* @param array  $atts    Attributes from the shortcode
+	* @param string $content Content on the page/post
+	*/	
+	public function performAdShortCode($atts, $content = null)
+	{
+		$this->_options = $this->getOptions();
+		extract($this->shortCodeExtract($atts, $this->_shortcode));
+		
+		$fallback = array();
+		if ($utg)
+		{
+			$fallback = $this->getTags();
+		}
+
+		if($q)
+		{
+			$newFallback = explode(',', $q);
+			foreach ($newFallback as $fall)
+			{
+				$fall = strtolower(trim($fall));
+				$fallback[] = $fall;
+			}
+		}
+
+		if($utt)
+		{
+			$fallback[] = strtolower(get_the_title());
+		}
+
+		if ($this->_options['Remove_Tags'])
+		{
+			$fallback = $this->removeTags($fallback);		
+		}
+
+		$fallback = implode(",", $fallback);
+
+		$height = $h ? ($h == 'auto' ? '100%' : preg_replace('/px|em|%/i', '', $h) . 'px') : 90 . 'px';
+		$width = $w ? ($w == 'auto' ? '100%' : preg_replace('/px|em|%/i', '', $w) . 'px') : '100%';
+
+		if ($options['prosperSid'] || $options['prosperSidText'])
+		{
+			$sid = '';
+			foreach ($options['prosperSid'] as $sidPiece)
+			{
+				switch ($sidPiece)
+				{
+					case 'blogname':
+						$sid .= get_bloginfo('name');
+						break;
+					case 'interface':
+						$sid .= 'pa';
+						break;
+					case 'query':
+						$sid .= $fallback;
+						break;
+					case 'page':
+						$sid .= get_the_title();
+						break;	
+				}
+			}
+			if (preg_match('/(^\$_(SERVER|SESSION|COOKIE))\[(\'|")(.+?)(\'|")\]/', $options['prosperSidText'], $regs))
+			{
+				if ($regs[1] == '$_SERVER')
+				{
+					$sid .= $_SERVER[$regs[4]];
+				}
+				elseif ($regs[1] == '$_SESSION')
+				{
+					$sid .= $_SESSION[$regs[4]];
+				}
+				elseif ($regs[1] == '$_COOKIE')
+				{
+					$sid .= $_COOKIE[$regs[4]];
+				}				
+			}
+		}		
+		
+		return '<div style="clear:both;"></div><p><div class="prosperent-pa" style="height:' . $height . '; width:' . $width . ';" pa_sid="' . $sid . '" pa_topics="' . $fallback . '"></div></p><div style="clear:both;"></div>';
+	}
+	
+	public function getTags()
+	{
+		$posttags = get_the_tags();
+		if ($posttags) 
+		{
+			foreach($posttags as $tag) 
+			{
+				$fallback[] = strtolower($tag->name); 
+			}
+		}	
+		
+		return $fallback;
+	}
+	
+	public function removeTags($fallback)
+	{
+		$removeTags = explode(',', $this->_options['Remove_Tags']);			
+		$fbacks = array_flip($fallback);
+
+		foreach ($removeTags as $remove)
+		{ 
+			$remove = trim($remove);
+			if(isset($fbacks[$remove]))
+			{
+				unset($fbacks[$remove]);
+			}
+		}	
+		
+		return array_flip($fbacks);			
+	}
+}

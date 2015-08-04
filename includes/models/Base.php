34ed57d6e01036ba127be3ce06905e5f53f39add
Prosperent <Prosperent@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-08-04 19:49:32 +0000

    = 4.2.1 = * Removed the ob_start("gzhandler"), some sites were not resolving when this was applied.
    git-svn-id: https://plugins.svn.wordpress.org/prosperent-suite/trunk@1212869 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- includes/models/Base.php
+++ includes/models/Base.php
@@ -449,14 +449,7 @@ abstract class Model_Base
 	
 	public function doOutputBuffer()
 	{
-        if (substr_count($_SERVER['HTTP_ACCEPT_ENCODING'], 'gzip'))
-        {
-            ob_start('ob_gzhandler');
-        }
-        else
-        {
             ob_start();
-        }
 	}
 	
 	public function getSid($settings = array(), $widget = array())
